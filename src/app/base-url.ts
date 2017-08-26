4ca292c78d5cec09ab7bf6ef822273d859fe816e
JOLEE <leejung209@gmail.com>
2017-08-27 01:34:18 +0900

    Automate Prod Url

--- src/app/base-url.ts
+++ src/app/base-url.ts
@@ -1,10 +1,10 @@
-import { isDevMode } from '@angular/core';
-
+import { enableProdMode, isDevMode } from '@angular/core';
+import { environment } from '../environments/environment';
 
 export const baseUrl: string = setBaseUrl();
 
 function setBaseUrl(){
-  if(isDevMode()){
+  if(!environment.production){
     return 'http://jbl-api-devel.snumedu.net:5000'
   } else {
     return 'http://jbl-api.snumedu.net:5000'
