2e022dda3be7aec49a673435354733b388f58522
JOLEE <leejung209@gmail.com>
2017-08-27 01:35:32 +0900

    Problem form working, Base format

--- src/app/problem/problem-form.component.ts
+++ src/app/problem/problem-form.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { FormBuilder, FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
 import { ProblemService } from '../problem.service';
-import { Problem, courseList, topicList } from './problem';
+import { Problem, yearList, courseList, topicList } from './problem';
 @Component({
   selector: 'problem-form',
   templateUrl: './problem-form.component.html',
@@ -10,51 +10,140 @@ import { Problem, courseList, topicList } from './problem';
 export class ProblemFormComponent implements OnInit {
 
   problemForm: FormGroup;
-  problem: Problem;
+  problemInfoForm;
+  @Input() problem: Problem;
+  newProblem: Problem;
+  yearList = yearList;
   courseList = courseList;
   topicList = topicList;
+  profList;
 
   constructor(
     private fb: FormBuilder,
     private problemService: ProblemService,
-  ){
-    this.createForm()
+  ) {
+    this.createForm();
+    // problem-info: year -> course -> topic -> prof
+
   }
 
-  ngOnInit(){
+
+
+  ngOnInit() {
 
   }
 
   ngOnChanges() {
-  this.problemForm.reset({
-    // topic: this.problem.topic,
-    // course: this.problem.course
-  })
-  }
-
-  createForm(){
-    this.problemForm = this.fb.group({
-      topic: ['', Validators.required],
-      course: ['', Validators.required],
-      year: ['', Validators.required],
-      answer: ['', Validators.required],
-      tags: [],
-      question: ['', Validators.required],
-      commentsCount: 0,
-      number: '',
+    this.problemForm.reset({
+      // topic: this.problem.topic,
+      // course: this.problem.course
     })
   }
 
-  onSubmit(){
-    this.problem = this.problemForm.value;
-    this.problemForm.reset()
-    this.problemService.addProblem(this.problem);
+  createForm() {
+    this.problemForm = this.fb.group({
+      info: this.fb.group({
+        topic: ['', Validators.required],
+        course: ['', Validators.required],
+        year: ['', Validators.required],
+        profs: this.fb.array([['',Validators.required]])
+      }),
+      question: ['', Validators.required],
+      answer: ['', Validators.required],
 
+
+      otherTags: this.fb.array([]),
+      commentsCount: 0,
+      numbers: ['', Validators.pattern('^[^0].*')],
+    });
+
+    this.problemInfoForm = this.problemForm.get('info');
+    this.problemService.findInfo(this.problemInfoForm.value)
+      .subscribe(res => { this.yearList = res })
   }
-  revert(){
+
+  setOtherTags(tags: string[]) {
+    const tagFGs = tags.map(tag => this.fb.group({ body: tag }));
+    const tagFormArray = this.fb.array(tagFGs);
+    this.problemForm.setControl('tags', tagFormArray);
+  }
+
+  addTag() {
+    this.otherTags.push(this.fb.group({ body: '' }));
+  }
+  addProf() {
+    this.profs.push(this.fb.control(''));
+  }
+
+  // Getting Select Box List //
+  updateCourseList() {
+    const courseControl = this.problemInfoForm.get('course');
+    const topicControl = this.problemInfoForm.get('topic');
+    topicControl.setValue('');
+    courseControl.setValue('');
+    this.problemService.findInfo(this.problemInfoForm.value)
+      .subscribe(res => { this.courseList = res })
+  }
+
+  updateTopicList() {
+    const topicControl = this.problemInfoForm.get('topic');
+    topicControl.setValue('');
+    this.problemService.findInfo(this.problemInfoForm.value)
+      .subscribe(res => { this.topicList = res })
+  }
+
+  updateProfList() {
+    // const profsControl = this.problemInfoForm.get('profs');
+    // profsControl.setValue('');
+    this.problemService.findInfo(this.problemInfoForm.value)
+      .subscribe(res => { this.profList = res })
+  }
+
+
+  // Submit or Cancel //
+  onSubmit() {
+    this.newProblem = this.prepareSave();
+    this.problemForm.reset();
+    console.log(this.newProblem);
+    this.problemService.addProblem(this.newProblem);
+  }
+
+  revert() {
     this.ngOnChanges();
   }
+
+  prepareSave(): Problem {
+    const formModel = this.problemForm.value;
+    const otherTagsDeepCopy: string[] = formModel.otherTags.map(
+      (tag) => tag.body
+    );
+    const saveProblem: Problem = {
+      id: this.problem? this.problem.id : {} ,
+      topic: formModel.info.topic,
+      course: formModel.info.course,
+      year: formModel.info.year,
+      profs: formModel.info.profs,
+      question: formModel.question,
+      answer: formModel.question,
+      otherTags: otherTagsDeepCopy,
+      numbers: formModel.numbers.replace(/\s+/g, "").split(","),
+      commentsCount: 0
+    };
+    return saveProblem;
+  }
+
+
   get answer() { return this.problemForm.get('answer'); }
+  get numbers() { return this.problemForm.get('numbers'); }
+  get otherTags(): FormArray {
+    return this.problemForm.get('otherTags') as FormArray;
+  };
+  get profs(): FormArray {
+    return this.problemForm.get('info.profs') as FormArray;
+  };
+
+
+
 
 
 }
--- src/app/problem/problem.ts
+++ src/app/problem/problem.ts
@@ -3,13 +3,16 @@ export class Problem {
   course: string ='';
 	year: string ='';
   answer: string ='';
-  tags: string[] =[];
+  profs: string[]=[];
+  otherTags: string[] =[];
 	question: string ='';
-  id: any; // 뭘로 해야하지..?
+  id: Object ={}; // 뭘로 해야하지..?
   commentsCount: number =0;
-  number: string='';
+  numbers: string[]=[];
 }
 
 export const courseList =["병리학", "면역학", "이외"];
 
 export const topicList =["topic1", "topic2", "topic3"];
+
+export const yearList =["2017", "2016", "2015", "2014"];
