1cdb2118916309c74c799deefc7e050ee2fb5817
JOLEE <leejung209@gmail.com>
2017-09-14 03:21:00 +0900

    Checkpoint for deletion/ Prevent unappropriate approach to empty toc

--- src/app/toc/toc.component.ts
+++ src/app/toc/toc.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TocService } from '../toc.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'toc',
@@ -14,20 +14,36 @@ export class TocComponent implements OnInit{
 
   constructor(
     private tocService: TocService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ){
     this.route.params
       .subscribe( param =>
         {
           this.tocService.getToc(param.course)
             .subscribe(res => {
-              this.toc = res;
-              console.log(this.toc);
+              if(!res){
+                alert("잠시 사용할 수 없거나, 없는 수업목록 입니다. ");
+                this.router.navigateByUrl("/toc");
+              } else {
+                this.toc = res;
+                console.log(this.toc);
+              }
             })
         })
   }
 
   ngOnInit(){
+  }
+
+  deleteToc(course){
+    if(confirm("정말 삭제하시겠습니까?")){
+      this.tocService.deleteToc(course)
+        .subscribe(res => {
+          this.router.navigateByUrl("/toc");
+          console.log("Delete succeeded:", res);
+        })
+    }
   }
 
 }
