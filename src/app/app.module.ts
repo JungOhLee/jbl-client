de53e2f78db2c9e8b45b5136ef8076c47d0f2a4b
JOLEE <leejung209@gmail.com>
2017-09-28 00:27:06 +0900

    Animation module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //Routing Module
 import { AppRoutingModule } from './app-routing.module';
