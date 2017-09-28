56e05dc8e59fbbf50f9e4861e8543702af8752a0
JOLEE <leejung209@gmail.com>
2017-09-29 00:03:10 +0900

    Csv download badge working well

--- src/app/jbl-main/jbl-main.component.ts
+++ src/app/jbl-main/jbl-main.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TocService } from '../toc.service';
 import { AuthService } from '../login/auth.service';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-jbl-main',
@@ -15,7 +16,8 @@ export class JblMainComponent implements OnInit {
   constructor(
     private router: Router,
     private tocService: TocService,
-    private authService: AuthService
+    private authService: AuthService,
+    private searchService: SearchService
   ) {
       this.tocService.getAllTocs().subscribe(res => {
         this.tocList = res;
@@ -35,5 +37,22 @@ export class JblMainComponent implements OnInit {
   }
   checkAdmin(){
     return this.authService.checkAdmin();
+  }
+
+  getCsv(course){
+    event.stopPropagation();
+    this.searchService.download(course)
+      .subscribe(res => {
+        this.download(course, res)
+      })
+  }
+
+  download(course, contents){
+    var link = document.createElement("a");
+    link.download = course + ".csv";
+    link.href = 'data:attachment/csv;charset=utf-8,' + encodeURI(contents);
+    link.target = '_blank';
+    document.body.appendChild(link);
+    link.click();
   }
 }
