8a1ff73be199f9032edf1ef5478b1dcef36156c5
JOLEE <leejung209@gmail.com>
2017-10-20 18:20:40 +0900

    checking edit/delete authentication added

--- src/app/problem/problem.component.ts
+++ src/app/problem/problem.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router} from '@angular/router';
 import { Location } from '@angular/common';
 import { BookmarkService } from '../bookmark.service';
 import { Bookmark } from '../bookmark';
+import { AuthService } from '../login/auth.service';
 import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import * as $ from 'jquery';
@@ -25,6 +26,7 @@ export class ProblemComponent implements OnInit {
   constructor(
     private problemService: ProblemService,
     private bookmarkService: BookmarkService,
+    private authService: AuthService,
     private route: ActivatedRoute,
     private router: Router,
     private location: Location
@@ -39,6 +41,11 @@ export class ProblemComponent implements OnInit {
   }
   closeMenu() {
     this.showMenu = false
+  }
+
+  isAuth() {
+    let logic = (this.authService.getUserEmail==this.problem.email) || (this.authService.checkAdmin)
+    return logic
   }
 
   toggleAnswer(){
@@ -72,6 +79,13 @@ export class ProblemComponent implements OnInit {
     return this.bookmarkService.deleteBookmark(bookmark)
   }
 
+  editeProblem(problem) {
+    if(this.isAuth()){
+      this.router.navigate(['/problem', problem.id, 'edit']);
+    } else {
+
+    }
+  }
   deleteProblem(problem) {
     if(confirm("정말 문제를 지우시겠습니까?")){
       this.problemService.deleteProblem(problem.id)
